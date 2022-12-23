b0b0b312e9a78640f6c4315729581e4b44d5a592
Ruan-Azeredo <ruanazeredo@gmail.com>
2022-12-23 14:22:00 -0300

    :tada: first commit

---
+++ src/test/java/com/explore/explore/ApiExploreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explore.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExploreSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
